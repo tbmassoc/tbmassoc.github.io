2510647709e888068cee7a33e2766c24f9cd75dc
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2026-04-04 09:38:52 +0800

    some additions have to be made. (with vtNet)

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -10,20 +10,42 @@ function applyThemeOnLoad() {
       document.querySelector(".themeChanger").onclick = () => { manualThemer("dark"); };
       document.querySelector(".themeChanger").title = "switch to dark mode";
 
-      // home page
+      document.querySelector("meta[name='theme-color']").setAttribute("content", "#dcc6bb");
+      document.querySelector("meta[name='msapplication-TileColor']").setAttribute("content", "#dcc6bb");
+      document.querySelector("meta[name='msapplication-navbutton-color']").setAttribute("content", "#dcc6bb");
+      document.querySelector("meta[name='apple-mobile-web-app-status-bar-style']").setAttribute("content", "#dcc6bb");
+
+      // root header
+      try { document.querySelector(".laLunaLogo").src = `/assets/svg/luna_${theme}.svg`; }
+      catch (error) { console.warn("no applicable resources found. skipping."); }
+      
+      // La Luna 🌙 (logo)
       try {
-        document.querySelector(".laLunaLogo").src = "/assets/svg/luna_light.svg";
+        document.querySelector(".laLunaLogoFull").src = `/assets/svg/tbmassoc_${theme}.svg`;
         document.querySelector(".laLunaHero").style.backgroundImage = "url('/assets/images/background/davies-designs-studio-f5_lfi2S-d4-unsplash.jpg')";
-        document.querySelector(".laLunaLogoFull").src = "/assets/svg/laLuna_light.svg";
+      }
+      catch (error) { console.warn("no applicable resources found. skipping."); }
+      
+      // vtNet (logo)
+      try {
+        document.querySelector(".vtNetLogoFull").src = "/assets/images/logo/vtNet_logo_light.png";
+        document.querySelector(".laLunaHero").style.backgroundImage = "url('/assets/images/background/davies-designs-studio-f5_lfi2S-d4-unsplash.jpg')";
       }
       catch (error) { console.warn("no applicable resources found. skipping."); }
 
       // sub-page articles
-      try {
-        document.querySelector(".subPageHeader").style.backgroundImage = "url('/assets/images/background/davies-designs-studio-f5_lfi2S-d4-unsplash.jpg')";
-      } catch (error) { console.warn("no applicable resources found. skipping."); }
+      try { document.querySelector(".subPageHeader").style.backgroundImage = "url('/assets/images/background/davies-designs-studio-f5_lfi2S-d4-unsplash.jpg')"; }
+      catch (error) { console.warn("no applicable resources found. skipping."); }
 
-      finally { return localStorage.setItem("theme", theme); }
+      finally {
+
+        // requires /vtNet/assets/js/theme.js
+        try { vtNetLogos(theme); }
+        catch (error) { console.warn("/vtNet/assets/js/theme.js not found. skipping.."); }
+
+        return localStorage.setItem("theme", theme);
+
+      }
 
     case "dark":
 
@@ -33,20 +55,42 @@ function applyThemeOnLoad() {
       document.querySelector(".themeChanger").onclick = () => { manualThemer("light"); };
       document.querySelector(".themeChanger").title = "switch to light mode";
 
-      // home page
+      document.querySelector("meta[name='theme-color']").setAttribute("content", "#3d374c");
+      document.querySelector("meta[name='msapplication-TileColor']").setAttribute("content", "#3d374c");
+      document.querySelector("meta[name='msapplication-navbutton-color']").setAttribute("content", "#3d374c");
+      document.querySelector("meta[name='apple-mobile-web-app-status-bar-style']").setAttribute("content", "#3d374c");
+
+      // root header
+      try { document.querySelector(".laLunaLogo").src = `/assets/svg/luna_${theme}.svg`; }
+      catch (error) { console.warn("no applicable resources found. skipping."); }
+      
+      // La Luna 🌙 (logo)
       try {
-        document.querySelector(".laLunaLogo").src = "/assets/svg/luna_dark.svg";
+        document.querySelector(".laLunaLogoFull").src = `/assets/svg/tbmassoc_${theme}.svg`;
         document.querySelector(".laLunaHero").style.backgroundImage = "url('/assets/images/background/damien-dufour-66PqHexp9DQ-unsplash.jpg')";
-        document.querySelector(".laLunaLogoFull").src = "/assets/svg/laLuna_dark.svg";
+      }
+      catch (error) { console.warn("no applicable resources found. skipping."); }
+      
+      // vtNet (logo)
+      try {
+        document.querySelector(".vtNetLogoFull").src = "/assets/images/logo/vtNet_logo_dark.png";
+        document.querySelector(".laLunaHero").style.backgroundImage = "url('/assets/images/background/damien-dufour-66PqHexp9DQ-unsplash.jpg')";
       }
       catch (error) { console.warn("no applicable resources found. skipping."); }
 
       // sub-page articles
-      try {
-        document.querySelector(".subPageHeader").style.backgroundImage = "url('/assets/images/background/damien-dufour-66PqHexp9DQ-unsplash.jpg')";
-      } catch (error) { console.warn("no applicable resources found. skipping."); }
+      try { document.querySelector(".subPageHeader").style.backgroundImage = "url('/assets/images/background/damien-dufour-66PqHexp9DQ-unsplash.jpg')"; }
+      catch (error) { console.warn("no applicable resources found. skipping."); }
 
-      finally { return localStorage.setItem("theme", theme); }
+      finally {
+
+        // requires /vtNet/assets/js/theme.js
+        try { vtNetLogos(theme); }
+        catch (error) { console.warn("/vtNet/assets/js/theme.js not found. skipping.."); }
+
+        return localStorage.setItem("theme", theme);
+
+      }
 
     default:
 
@@ -55,20 +99,42 @@ function applyThemeOnLoad() {
       document.querySelector(".themeChanger").onclick = () => { manualThemer("dark"); };
       document.querySelector(".themeChanger").title = "switch to dark mode";
 
-      // home page
+      document.querySelector("meta[name='theme-color']").setAttribute("content", "#dcc6bb");
+      document.querySelector("meta[name='msapplication-TileColor']").setAttribute("content", "#dcc6bb");
+      document.querySelector("meta[name='msapplication-navbutton-color']").setAttribute("content", "#dcc6bb");
+      document.querySelector("meta[name='apple-mobile-web-app-status-bar-style']").setAttribute("content", "#dcc6bb");
+
+      // root header
+      try { document.querySelector(".laLunaLogo").src = `/assets/svg/luna_light.svg`; }
+      catch (error) { console.warn("no applicable resources found. skipping."); }
+      
+      // La Luna 🌙 (logo)
       try {
-        document.querySelector(".laLunaLogo").src = "/assets/svg/luna_light.svg";
+        document.querySelector(".laLunaLogoFull").src = `/assets/svg/tbmassoc_light.svg`;
         document.querySelector(".laLunaHero").style.backgroundImage = "url('/assets/images/background/davies-designs-studio-f5_lfi2S-d4-unsplash.jpg')";
-        document.querySelector(".laLunaLogoFull").src = "/assets/svg/laLuna_light.svg";
+      }
+      catch (error) { console.warn("no applicable resources found. skipping."); }
+      
+      // vtNet (logo)
+      try {
+        document.querySelector(".vtNetLogoFull").src = "/assets/images/logo/vtNet_logo_light.png";
+        document.querySelector(".laLunaHero").style.backgroundImage = "url('/assets/images/background/davies-designs-studio-f5_lfi2S-d4-unsplash.jpg')";
       }
       catch (error) { console.warn("no applicable resources found. skipping."); }
 
       // sub-page articles
-      try {
-        document.querySelector(".subPageHeader").style.backgroundImage = "url('/assets/images/background/davies-designs-studio-f5_lfi2S-d4-unsplash.jpg')";
-      } catch (error) { console.warn("no applicable resources found. skipping."); }
+      try { document.querySelector(".subPageHeader").style.backgroundImage = "url('/assets/images/background/davies-designs-studio-f5_lfi2S-d4-unsplash.jpg')"; }
+      catch (error) { console.warn("no applicable resources found. skipping."); }
 
-      finally { return localStorage.setItem("theme", "light"); }
+      finally {
+
+        // requires /vtNet/assets/js/theme.js
+        try { vtNetLogos("light"); }
+        catch (error) { console.warn("/vtNet/assets/js/theme.js not found. skipping.."); }
+
+        return localStorage.setItem("theme", "light");
+
+      }
 
   }
 }
@@ -84,20 +150,42 @@ function manualThemer(theme) {
       document.querySelector(".themeChanger").onclick = () => { manualThemer("dark"); };
       document.querySelector(".themeChanger").title = "switch to dark mode";
 
-      // main page
+      document.querySelector("meta[name='theme-color']").setAttribute("content", "#dcc6bb");
+      document.querySelector("meta[name='msapplication-TileColor']").setAttribute("content", "#dcc6bb");
+      document.querySelector("meta[name='msapplication-navbutton-color']").setAttribute("content", "#dcc6bb");
+      document.querySelector("meta[name='apple-mobile-web-app-status-bar-style']").setAttribute("content", "#dcc6bb");
+
+      // root header
+      try { document.querySelector(".laLunaLogo").src = `/assets/svg/luna_${theme}.svg`; }
+      catch (error) { console.warn("no applicable resources found. skipping."); }
+      
+      // La Luna 🌙 (logo)
       try {
-        document.querySelector(".laLunaLogo").src = "/assets/svg/luna_light.svg";
+        document.querySelector(".laLunaLogoFull").src = `/assets/svg/tbmassoc_${theme}.svg`;
         document.querySelector(".laLunaHero").style.backgroundImage = "url('/assets/images/background/davies-designs-studio-f5_lfi2S-d4-unsplash.jpg')";
-        document.querySelector(".laLunaLogoFull").src = "/assets/svg/laLuna_light.svg";
+      }
+      catch (error) { console.warn("no applicable resources found. skipping."); }
+      
+      // vtNet (logo)
+      try {
+        document.querySelector(".vtNetLogoFull").src = "/assets/images/logo/vtNet_logo_light.png";
+        document.querySelector(".laLunaHero").style.backgroundImage = "url('/assets/images/background/davies-designs-studio-f5_lfi2S-d4-unsplash.jpg')";
       }
       catch (error) { console.warn("no applicable resources found. skipping."); }
 
       // sub-page articles
-      try {
-        document.querySelector(".subPageHeader").style.backgroundImage = "url('/assets/images/background/davies-designs-studio-f5_lfi2S-d4-unsplash.jpg')";
-      } catch (error) { console.warn("no applicable resources found. skipping."); }
+      try { document.querySelector(".subPageHeader").style.backgroundImage = "url('/assets/images/background/davies-designs-studio-f5_lfi2S-d4-unsplash.jpg')"; }
+      catch (error) { console.warn("no applicable resources found. skipping."); }
 
-      finally { return localStorage.setItem("theme", theme); }
+      finally {
+
+        // requires /vtNet/assets/js/theme.js
+        try { vtNetLogos(theme); }
+        catch (error) { console.warn("/vtNet/assets/js/theme.js not found. skipping.."); }
+
+        return localStorage.setItem("theme", theme);
+
+      }
 
     case "dark":
 
@@ -107,20 +195,42 @@ function manualThemer(theme) {
       document.querySelector(".themeChanger").onclick = () => { manualThemer("light"); };
       document.querySelector(".themeChanger").title = "switch to light mode";
 
-      // main page
+      document.querySelector("meta[name='theme-color']").setAttribute("content", "#3d374c");
+      document.querySelector("meta[name='msapplication-TileColor']").setAttribute("content", "#3d374c");
+      document.querySelector("meta[name='msapplication-navbutton-color']").setAttribute("content", "#3d374c");
+      document.querySelector("meta[name='apple-mobile-web-app-status-bar-style']").setAttribute("content", "#3d374c");
+
+      // root header
+      try { document.querySelector(".laLunaLogo").src = `/assets/svg/luna_${theme}.svg`; }
+      catch (error) { console.warn("no applicable resources found. skipping."); }
+      
+      // La Luna 🌙 (logo)
       try {
-        document.querySelector(".laLunaLogo").src = "/assets/svg/luna_dark.svg";
+        document.querySelector(".laLunaLogoFull").src = `/assets/svg/tbmassoc_${theme}.svg`;
         document.querySelector(".laLunaHero").style.backgroundImage = "url('/assets/images/background/damien-dufour-66PqHexp9DQ-unsplash.jpg')";
-        document.querySelector(".laLunaLogoFull").src = "/assets/svg/laLuna_dark.svg";
+      }
+      catch (error) { console.warn("no applicable resources found. skipping."); }
+      
+      // vtNet (logo)
+      try {
+        document.querySelector(".vtNetLogoFull").src = "/assets/images/logo/vtNet_logo_dark.png";
+        document.querySelector(".laLunaHero").style.backgroundImage = "url('/assets/images/background/damien-dufour-66PqHexp9DQ-unsplash.jpg')";
       }
       catch (error) { console.warn("no applicable resources found. skipping."); }
 
       // sub-page articles
-      try {
-        document.querySelector(".subPageHeader").style.backgroundImage = "url('/assets/images/background/damien-dufour-66PqHexp9DQ-unsplash.jpg')";
-      } catch (error) { console.warn("no applicable resources found. skipping."); }
+      try { document.querySelector(".subPageHeader").style.backgroundImage = "url('/assets/images/background/damien-dufour-66PqHexp9DQ-unsplash.jpg')"; }
+      catch (error) { console.warn("no applicable resources found. skipping."); }
 
-      finally { return localStorage.setItem("theme", theme); }
+      finally {
+
+        // requires /vtNet/assets/js/theme.js
+        try { vtNetLogos(theme); }
+        catch (error) { console.warn("/vtNet/assets/js/theme.js not found. skipping.."); }
+
+        return localStorage.setItem("theme", theme);
+
+      }
 
     default: return;
 
